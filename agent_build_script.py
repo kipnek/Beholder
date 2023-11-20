fa54471209415f5e490aa06b96bc486ae8ba676e
sigh-gone <alexwhite1964@icloud.com>
2023-11-20 13:56:27 -0600

    build script

---
+++ agent_build_script.py
@@ -0,0 +1,22 @@
+import os
+
+
+def build_rust_binary(home, id):
+    # Read the template
+    with open('./agent/src/agent_config.rs.tmpl', 'r') as file:
+        template = file.read()
+
+    # Replace placeholders
+    source_code = template.replace("{{ID}}", id)
+    source_code = template.replace("{{HOME}}", home)
+
+    # Write to a source file
+    with open('./agent/src/agent_config.rs', 'w') as file:
+        file.write(source_code)
+
+    # Compile the binary
+    os.system('cargo build --release --manifest-path=./agent/Cargo.toml')
+
+
+# Example usage
+build_rust_binary("127.0.0.1:80", "12345")
