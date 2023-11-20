f719a07809bc9343d7056f9fb3bd8a4e4cf3c9b9
sigh-gone <alexwhite1964@icloud.com>
2023-11-20 13:44:29 -0600

    added a little more

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -9,16 +9,21 @@ use crate::config::AgentConfig;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let agent_config = get_agent_config();
-    let agent = Agent::new(agent_config);
-
-    loop{
-
-
-        break;
+    let mut agent = Agent::new(agent_config);
+    if initialize().is_ok() && agent.ping_home().await.is_ok(){
+        work(agent).await;
     }
     Ok(())
 }
 
-fn initialize(config:AgentConfig) -> agent::Agent{
-    Agent::new(config)
+fn initialize() -> Result<(), String>{
+    //initialize the agent
+    Ok(())
+}
+
+async fn work(mut agent:Agent){
+    loop{
+        //do jobs logic
+        break;
+    }
 }
